1b4f0baa45132fceb3a04bd7bc79caf70989af3c
delaemon <takeshi.music.5@gmail.com>
2015-09-09 22:37:48 +0900

    Add generate salt

--- scramble.go
+++ scramble.go
@@ -1,22 +1,80 @@
 package scramble
+import (
+	"math/rand"
+	"time"
+	"fmt"
+	"errors"
+)
 
-func trim(v int) int {
-	return v & 0xffffffff
+func GenerateSalt() (salt, inverse int) {
+	salt = generateSalt()
+	inverse = modInverse(salt, 0x100000000)
+	return
 }
 
-func scrabmble(v int) int {
-	v ^= 0x1ca7bc5b
-	v *= 0x1ca7bc5b
-	v = trim(v)
+func generateSalt() int {
+	rand.Seed(time.Now().Unix())
+	salt := rand.Intn(0xFFFFFFFF - 3) + 3
+	if salt % 2 == 0 {
+		salt += 1
+	}
+	return salt
+}
 
-	v = ((v >> 1) & 0x55555555) | trim((v & 0x55555555) << 1)
-	v = ((v >> 2) & 0x33333333) | trim((v & 0x33333333) << 2)
-	v = ((v >> 4) & 0x0F0F0F0F) | trim((v & 0x0F0F0F0F) << 4)
-	v = ((v >> 8) & 0x00FF00FF) | trim((v & 0x00FF00FF) << 8)
-	v = ( v >> 16)              | trim(v                << 16)
+func egcd(a, b int) (int, int, int) {
+	if a == 0 {
+		return b, 0, 1
+	} else {
+		g, y, x := egcd(b % a, a)
+		return g, (x - (b/a) * y), y
+	}
 
-	v *= 0x6b5f13d3
-	v = trim(v)
-	v ^= 0x1ca7bc5b
-	return v
+}
+
+func modInverse(a, m int) int {
+	g, x, _ := egcd(a, m)
+	if g != 1 {
+		panic("inverse does not exist.")
+	}
+	return x % m
+}
+
+func assertNumber(num int) bool {
+	return (1 <= num && num <= 0xFFFFFFFF)
+}
+
+func assertSalt(salt, inverse int) bool {
+	return trim(salt * inverse) == 1
+}
+
+func trim(num int) int {
+	return num & 0xFFFFFFFF
+}
+
+func reverse32bit(num int) int {
+	num = ((num >> 1) & 0x55555555) | trim((num & 0x55555555) << 1)
+	num = ((num >> 2) & 0x33333333) | trim((num & 0x33333333) << 2)
+	num = ((num >> 4) & 0x0F0F0F0F) | trim((num & 0x0F0F0F0F) << 4)
+	num = ((num >> 8) & 0x00FF00FF) | trim((num & 0x00FF00FF) << 8)
+	num = ( num >> 16)              | trim( num               << 16)
+	return num
+}
+
+func Scrabmble(num, salt, inverse int) (int, error) {
+	if !assertNumber(num) {
+		e := fmt.Sprintf("invalid number. num = %d", num)
+		return -1, errors.New(e)
+	}
+	if !assertSalt(salt, inverse) {
+		e := fmt.Sprintf("invalid salt. salt = %d, inverse: %d",salt ,inverse)
+		return -1, errors.New(e)
+	}
+
+	num ^= salt
+	num = trim(num * salt)
+	num = reverse32bit(num)
+	num *= inverse
+	num = trim(num)
+	num ^= salt
+	return num, nil
 }
--- scramble_test.go
+++ scramble_test.go
@@ -5,12 +5,21 @@ import (
 )
 
 func TestScramble(t *testing.T) {
-	for in := 0; in < 1000; in++ {
-		scr := scrabmble(in)
-		out := scrabmble(scr)
-		fmt.Printf("in: %d, scr: %d, out: %d\n",in, scr, out)
-		if in != out {
-			t.Errorf("i != scramble(scramble(i)), in: %d, scr:%d, out: %d", in, scr, out)
+	for i := 0; i < 10000; i++ {
+		salt, inverse := GenerateSalt()
+		for in := 1; in < 10000; in++ {
+			scr, err := Scrabmble(in, salt, inverse)
+			if err != nil {
+				t.Errorf(fmt.Sprintf("%s",err))
+			}
+			out, err := Scrabmble(scr, salt, inverse)
+			if err != nil {
+				t.Errorf(fmt.Sprintf("%s",err))
+			}
+			if in != out {
+				t.Errorf("i != scramble(scramble(i)),salt: %d, inverse: %d, in: %d, scr:%d, out: %d",
+					salt, inverse, in, scr, out)
+			}
 		}
 	}
 }
